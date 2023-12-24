2e130f570ee494f221964af8745692119b2be49f
ali <alihoosinyb@gmail.com>
2023-12-24 13:46:55 +0330

    1.1.0

--- js/runner.js
+++ js/runner.js
@@ -2,18 +2,26 @@ const form = document.querySelector("#form");
 const title = document.querySelector("#title");
 const price = document.querySelector("#price");
 const products = document.querySelector("#products");
-const addFile = document.querySelector(".file");
+const inputPics = document.querySelector(".pics")
 const addProduct = (e) => {
   e.preventDefault();
   if (title.value == "" || price.value == "") {
     alert("اطاعات محصول را وارد کنید");
     return;
   }
-  const productItem = document.createElement("li");
-  const productInfo = document.createTextNode(` title is :${title.value} price is : ${price.value} Rial `);
-  products.appendChild(productItem);
 
+  const productItem = document.createElement("div");
+  const images = document.createElement('img');
+  
+  const productInfo = document.createTextNode(
+    ` title is : ${title.value} price is : ${price.value} Rial `
+  );
+  productItem.classList.add("card" ,"col-3")
+  products.appendChild(productItem);
   productItem.appendChild(productInfo);
+  productItem.appendChild(link)
+  productItem.appendChild(images)
+  images.src = `${inputPics.value}`
   title.value = "";
   price.value = "";
 };
